50e0bade56869f6c6923788715aaa087e7fc9bb1
10000rooz <10000rooz@gmail.com>
2023-04-05 16:30:56 -0700

    fix: home page

--- src/constants/appMetaData.ts
+++ src/constants/appMetaData.ts
@@ -1,6 +1,6 @@
 export const brandName = '۱۰۰۰۰روز';
 export const brandNameEn = '10000rooz';
-export const brandTitle = "۱۰۰۰روز | راهنمای مسیر شغلی";
+export const SITE_TITLE = '۱۰۰۰روز | راهنمای مسیر شغلی';
 
-export const brandDescription = '۱۰۰۰۰روز';
+export const SITE_DESCRIPTION = '۱۰۰۰۰روز';
 export const websiteURL = 'https://10000rooz.com';
