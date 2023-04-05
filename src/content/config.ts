2157224e9c3200e687667dd20619321ec14b5b84
10000rooz <10000rooz@gmail.com>
2023-04-05 15:43:28 -0700

    feat: add content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,25 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    categories: z.array(z.string()),
+    tags: z.array(z.string()),
+    features: z.array(z.string()),
+    // Transform string to Date object
+    datePublished: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    dateUpdated: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    // heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
