38fa15c58e092aef3a26ebefbbe291007f3f1575
vashmeen <->
2024-03-09 16:27:08 +0330

    fix: translation

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -69,7 +69,7 @@ const TableOfContents: React.FC<{ headings: MarkdownHeading[] }> = ({
 	return (
 		<>
 			<h2 id={onThisPageID} className="heading">
-				On this page
+				فهرست
 			</h2>
 			<ul ref={toc}>
 				{headings
